5359ebf8cb2aa56ea9cbf0492a92681652b19e94
Sadan (Master Mode) <KevinPlayz1970@gmail.com>
2025-05-02 09:25:53 -0400

    Remove solution files

--- Unit-04/Chapter-02/04-02-01.py
+++
@@ -1,7 +0,0 @@
-lists = []
-with open("Unit-04/Chapter-02/problem txt files/pizza-toppings.txt", "r") as files:
-    for i in files:
-        lists.append(i.rstrip("\n"))
-lists.sort()
-for i in lists:
-    print(i)
--- Unit-04/Chapter-02/04-02-02.py
+++
@@ -1,5 +0,0 @@
-total = 0
-with open("Unit-04/Chapter-02/problem txt files/gme-buy.txt", "r") as files:
-    for i in files:
-        total+=(int(i.rstrip("\n")))
-print(total)
