4f0a05230ada73021d0a40c7c04e91a6e414e8e7
Sadan (Master Mode) <KevinPlayz1970@gmail.com>
2025-05-08 09:09:32 -0400

    04-01 to 04-06 test cases
    all work well

---
+++ Unit-04/Chapter-04/04-04-05.py
@@ -0,0 +1,4 @@
+inp = input()
+for i in range(len(inp) -1):
+    print(inp[i] + "-", end="")
+print(inp[-1])
---
+++ Unit-04/Chapter-04/04-04-06.py
@@ -0,0 +1,3 @@
+inp = input()
+
+print(str(inp.lower().find("e")) + ", " + str(inp.lower().rfind("e")))
