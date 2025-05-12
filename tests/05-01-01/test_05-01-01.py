95c677052e3b56b1553e2079b72e4ab6d2274679
bassemfarid <71142299+bassemfarid@users.noreply.github.com>
2025-05-12 09:21:15 -0400

    increased error verbosity

--- tests/05-01-01/test_05-01-01.py
+++ tests/05-01-01/test_05-01-01.py
@@ -33,5 +33,5 @@ class TestStudentSolution(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    runner = unittest.TextTestRunner(verbosity=0)
+    runner = unittest.TextTestRunner(verbosity=1)
     unittest.main(testRunner=runner)
